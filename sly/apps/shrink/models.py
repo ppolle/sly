2003df02efb57118d24a9c278e0723f5ef18e65d
ppolle <peter.m.polle@gmail.com>
2019-06-05 13:56:42 +0300

    updated sly model URL's

--- sly/apps/shrink/models.py
+++ sly/apps/shrink/models.py
@@ -3,3 +3,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class SlyUrl(models.Model):
+	'''Model tat saves Url info to be shortened'''
+	url = models.CharField(max_length=300)
+	short_code = models.CharField(max_length=50, blank=True)
+	timestamp = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.url
+
+	def encode(self, url):
+		'''shorten a url link'''
+		pass
+
+	def decode(self, url):
+		'''change a url back to original url'''
+		pass
+
