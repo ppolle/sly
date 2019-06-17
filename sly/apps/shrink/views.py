71c2732398f8d016319b8768b6b361ca72e3ceb2
ppolle <peter.m.polle@gmail.com>
2019-06-17 20:28:24 +0300

    this commit will update to CBV

--- sly/apps/shrink/views.py
+++ sly/apps/shrink/views.py
@@ -1,27 +1,61 @@
 from django.shortcuts import render, redirect
+from django.views.generic.base import View
 from .forms import UrlForm
 from .models import SlyUrl
 
 # Create your views here.
-def index(request):
-	'''
-	Method that displays shortens links
-	'''
-	if request.method == 'POST':
+class IndexView(View):
+	def get(self, request, *args, **kwargs):
+		'''
+		Form to dispaly the index page
+		'''
+		form = UrlForm()
+		return render(request, 'shrink/index.html', {'form':form})
+
+	def post(self, request, *args, **kwargs):
+		'''
+		Handle inedx page post requests
+		'''
 		form = UrlForm(request.POST)
+		template = 'shrink/index.html'
+		context = {'form':form}
+
 		if form.is_valid():
 			longUrl = form.cleaned_data['url']
 			shortCode = form.cleaned_data['short_code']
 
-			if shortCode is None:
-				url = SlyUrl.objects.create(longUrl = longUrl)		
+			if shortCode == '':
+				created, obj = SlyUrl(longUrl = longUrl)
+				obj.save()
+				obj.refresh_from_db()
+				obj.shortCode = self.generate_shortcode(url.id)
+				obj.save()
 			else:
-				url = SlyUrl.objects.create(longUrl=longUrl,shortCode=shortCode)
-			
-			return render(request, 'shrink/success.html', {'shortUrl':url.shortCode})
+				created, obj = SlyUrl.objects.create(longUrl=longUrl,shortCode=shortCode)
+				
+			if created:
+				template = 'shrink/success.html'
+				context = {
+						'object':obj					
+					}
 
-	else:
-		form = UrlForm()
+		return render(request, template, context)
 
-	return render(request, 'shrink/index.html', {'form':form})
+	def generate_shortcode(self, id):
+		'''
+		Generate urls shortcode
+		'''
+		import string
+		characters = string.digits+string.uppercase+string.lowercase
+		base = len(characters)
+		ret = []
+
+		while id > 0:
+			val = id % base
+			ret.append(characters[val])
+			id = id // base
+		
+		return "".join(ret[::-1])
+
+
 
