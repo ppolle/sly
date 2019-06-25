b6091428d94c76f3ce67e2651e7adcc9866ca0e0
ppolle <peter.m.polle@gmail.com>
2019-06-25 19:27:36 +0300

    this commit will update created_by field in SlyUrl models

--- sly/apps/shrink/models.py
+++ sly/apps/shrink/models.py
@@ -2,17 +2,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class SlyUrl(models.Model):
 	'''Model that saves Url info to be shortened'''
+	created_by = models.ForeignKey(User, related_name="slyurls", blank=True, on_delete=models.CASCADE)
 	longUrl = models.URLField(max_length=300)
 	shortCode = models.CharField(max_length=50, blank=True, unique=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
-		return self.url
+		return self.longUrl
 
 	def get_short_url(self):
 		'''get the shortcode url'''
