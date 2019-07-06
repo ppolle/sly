e0abbe26c9aadefcc3d7f4f27b0d7de941799835
ppolle <peter.m.polle@gmail.com>
2019-07-06 23:55:55 +0300

    fixed trailing comma issue

--- sly/apps/api/urls.py
+++ sly/apps/api/urls.py
@@ -3,6 +3,6 @@ from sly.apps.api.views import ShortCodeList, ShortCodeDetail
 
 urlpatterns = [
 	url(r'shortcode/$', ShortCodeList.as_view()),
-	url(r'shortcode/(?P<shortcode>[\w-]+)/$', ShortCodeDetail.as_view()),
+	url(r'shortcode/(?P<shortcode>[\w-]+)$', ShortCodeDetail.as_view()),
 
 ]
