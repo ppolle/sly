f40f47962dc4c1b65d134a23604fcdf956c41983
ppolle <peter.m.polle@gmail.com>
2020-01-10 15:31:40 +0300

    this commit will update extract_doamin faunctionality in validators.py

--- sly/apps/shrink/tests/test_validators.py
+++ sly/apps/shrink/tests/test_validators.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from sly.apps.shrink.validators import validate_url
+from sly.apps.shrink.validators import validate_url, extract_domain
 
 class ValidateUrlTests(TestCase):
 	def test_validate_url(self):
 		url = 'blah.blah'
 		self.assertRaises(ValidationError, validate_url, url)
+
+	def test_extract_domain(self):
+		url = 'https://www.youtube.com/watch?v=Btbvv9kfLqo'
+		uri = extract_domain(url)
+		self.assertEqual(uri, 'www.youtube.com')
--- sly/apps/shrink/validators.py
+++ sly/apps/shrink/validators.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
+from django.contrib.sites.models import Site
 
 def validate_url(value):
 	'''
@@ -12,3 +13,18 @@ def validate_url(value):
 		raise ValidationError("This field has to be a proper URL")
 
 	return value
+
+def omit_own_domain(value):
+	current_site = Site.objects.get_current().lower().strip()
+	incoming_site = extract_domain(value).lower().strip()
+
+	if current_site is not incoming_site:
+		raise ValidationError("You cannot shorten a URL from this site")
+	else:
+		return value
+
+def extract_domain(value):
+	from urlparse import urlparse
+
+	uri = urlparse(value)
+	return uri.netloc
